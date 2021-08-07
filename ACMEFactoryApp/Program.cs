ce2c64cdc6dcff277be5546c1d3204463307a3a9
Patrick Andrews M <Patrickandrews.M@Ltts.com>
2021-08-07 11:16:26 +0530

    Added Console Output for ease of flow of operation

--- ACMEFactoryApp/Program.cs
+++ ACMEFactoryApp/Program.cs
@@ -10,6 +10,28 @@ namespace ACMEFactoryApp
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Welcome to ACME Factory at Tokyo");
+            //Console.WriteLine("Please login with you credentials");
+            ////LogicForFloorManager or Regular Worker
+            //if (/*FloorManager*/)
+            //{
+            //    Console.WriteLine("Choose what activity do you want to perform");
+            //    Console.WriteLine("1.Get all employee working hours report");
+            //    Console.WriteLine("2. Get all machine working hours report");
+            //}
+            //else if(/*Regular Worker*/)
+            //{
+            //    Console.WriteLine("Choose what activity do you want to perform");
+            //    Console.WriteLine("1. Log Start Time for the Shift");
+            //    Console.WriteLine("2. Log End Time for the Shift");
+            //    Console.WriteLine("3. Log Start Time for the machine");
+            //    Console.WriteLine("3. Log End Time for the machine");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are not authorised to login/Login credentials is incorrect");
+            //}
         }
     }
 }
