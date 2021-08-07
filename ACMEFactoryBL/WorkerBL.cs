d7fba2bc350e54df216272d019f5a1031bf497fb
Pranali21699 <87111027+Pranali21699@users.noreply.github.com>
2021-08-07 19:08:23 +0530

    Update WorkerBL.cs

--- ACMEFactoryBL/WorkerBL.cs
+++ ACMEFactoryBL/WorkerBL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using ACMEFactoryDAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,132 @@ using System.Threading.Tasks;
 
 namespace ACMEFactoryBL
 {
-    class WorkerBL
+   public class WorkerBL
     {
+        WorkerDAL obj;
+        public WorkerBL()
+        {
+            obj = new WorkerDAL();
+        }
+        //public List<DTO> Login(int id, string username, string password, out int rowAffected)
+        public List<ACMEFactoryDTO.DTO> LoginCheck(int id, string username, string password, out int rowAffected)
+        {
+            try
+            {
+                var ret = obj.Login(id, username, password, out rowAffected);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int InsertALoginTime(int Iplgtime, out int rowAffected)
+        {
+            try
+            {
+                var ret = obj.InsertAppLoginTime(Iplgtime,out rowAffected);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int InsertALogoutTime(int Iplgotime, out int rowAffected)
+        {
+            try
+            {
+                var ret = obj.InsertAppLogoutTime(Iplgotime, out rowAffected);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        //for m1
+        public int InsertALoginTimeForM1(int Iplgtime, out int rowAffected)
+        {
+            try
+            {
+                var ret = obj.InsertAppLoginTime(Iplgtime, out rowAffected);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int InsertALogoutTimeForM1(int Iplgotime, out int rowAffected)
+        {
+            try
+            {
+                var ret = obj.InsertAppLogoutTime(Iplgotime, out rowAffected);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //For m2
+        public int InsertALoginTimeForM2(int Iplgtime, out int rowAffected)
+        {
+            try
+            {
+                var ret = obj.InsertAppLoginTime(Iplgtime, out rowAffected);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int InsertALogoutTimeForM2(int Iplgotime, out int rowAffected)
+        {
+            try
+            {
+                var ret = obj.InsertAppLogoutTime(Iplgotime, out rowAffected);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        //for m3
+        public int InsertALoginTimeForM3(int Iplgtime, out int rowAffected)
+        {
+            try
+            {
+                var ret = obj.InsertAppLoginTime(Iplgtime, out rowAffected);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int InsertALogoutTimeForM3(int Iplgotime, out int rowAffected)
+        {
+            try
+            {
+                var ret = obj.InsertAppLogoutTime(Iplgotime, out rowAffected);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
     }
 }
